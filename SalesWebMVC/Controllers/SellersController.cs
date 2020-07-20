027efd924f0d6e098ce49cec4b63d1e616e8fd46
unknown <samuca_navarro@outlook.com>
2020-07-20 00:23:00 -0300

    Delete para vendedor

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -40,5 +40,30 @@ namespace SalesWebMVC.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete (int? id) //o ? significa q é opcional
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Destination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Cause { get; set; }
+
+        public Destination()
+        {
+
+        }
+
+        public Destination(int id, string cause)
+        {
+            Id = id;
+            Cause = cause;
+        }
+    }
+
+
+}
